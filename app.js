0fc79b376199cded183024200b9b1744022ef8a3
Sebastián Torres <seba.torresz@duocuc.cl>
2024-02-20 23:30:10 -0300

    entrega websockets

--- app.js
+++ app.js
@@ -1,173 +1,97 @@
 const express = require('express');
+const http = require('http');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const ProductManager = require('./src/ProductManager');
 const CartManager = require('./src/CartManager');
 const axios = require('axios');
 
 const app = express();
+const httpServer = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+    },
+    allowEIO3: true,
+    serveClient: true,
+    transports: ['websocket'],
+    httpCompression: true,
+    cookie: false,
+});
+
 const port = 8080;
 const serverUrl = `http://localhost:${port}`;
 
 const productManager = new ProductManager('./lista-productos.json');
 const cartManager = new CartManager('./carrito.json');
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader(
         'Content-Security-Policy',
-        "default-src 'none'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com"
+        "default-src 'none'; script-src 'self' http://localhost:8080 'unsafe-inline'; style-src 'self' https://fonts.googleapis.com 'unsafe-inline'; font-src 'self' https://fonts.gstatic.com; connect-src 'self' ws://localhost:8080 http://localhost:8080"
     );
     next();
+});
+
+app.get('/home', async (req, res) => {
+    try {
+        await productManager.loadProducts();
+        const products = productManager.getProducts();
+        res.render('home', { products });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener productos' });
+    }
+});
+
+app.get('/realTimeProducts', async (req, res) => {
+    try {
+        await productManager.loadProducts();
+        const products = productManager.getProducts();
+        res.render('realTimeProducts', { products });
+        io.emit('connectedClient', 'Cliente conectado');
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener productos en tiempo real' });
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log("Cliente conectado a través de Socket.io");
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    socket.on('addProduct', (productData) => {
+        try {
+            productManager.addProduct(productData);
+            io.emit('productAdded', productManager.getProducts());
+        } catch (error) {
+            console.error('Error al agregar producto:', error);
+        }
+    });
+
+    socket.on('deleteProduct', (productId) => {
+        try {
+            productManager.deleteProduct(productId);
+            io.emit('productDeleted', productId);
+        } catch (error) {
+            console.error('Error al eliminar producto:', error);
+        }
+    });
 });
 
 app.get('/', (req, res) => {
     res.send('¡Bienvenido a la aplicación!');
 });
 
-const productRouter = express.Router();
-
-productRouter.get('/', async (req, res) => {
-    const limit = req.query.limit;
-
-    try {
-        await productManager.loadProducts();
-        const products = limit ? productManager.getProducts().slice(0, limit) : productManager.getProducts();
-        res.json(products);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener productos' });
-    }
-});
-
-productRouter.get('/:pid', async (req, res) => {
-    const productId = parseInt(req.params.pid);
-
-    try {
-        await productManager.loadProducts();
-        const product = productManager.getProductById(productId);
-
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404).json({ error: 'Producto no encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener producto' });
-    }
-});
-
-productRouter.post('/', (req, res) => {
-    const productData = req.body;
-
-    try {
-        productManager.addProduct(productData);
-        res.status(201).json({ message: 'Producto agregado correctamente' });
-    } catch (error) {
-        res.status(400).json({ error: 'Error al agregar producto' });
-    }
-});
-
-productRouter.put('/:pid', (req, res) => {
-    const productId = parseInt(req.params.pid);
-    const updatedFields = req.body;
-
-    try {
-        productManager.updateProduct(productId, updatedFields);
-        res.json({ message: 'Producto actualizado correctamente' });
-    } catch (error) {
-        res.status(400).json({ error: 'Error al actualizar producto' });
-    }
-});
-
-productRouter.delete('/:pid', (req, res) => {
-    const productId = parseInt(req.params.pid);
-
-    try {
-        productManager.deleteProduct(productId);
-        res.json({ message: 'Producto eliminado correctamente' });
-    } catch (error) {
-        res.status(400).json({ error: 'Error al eliminar producto' });
-    }
-});
-
-app.use('/api/products', productRouter);
-
-const cartRouter = express.Router();
-
-cartRouter.post('/', async (req, res) => {
-    try {
-        const cartId = await cartManager.createCart();
-        res.json({ cartId });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al crear carrito' });
-    }
-});
-
-cartRouter.get('/:cid', async (req, res) => {
-    const cartId = req.params.cid;
-
-    try {
-        const cart = await cartManager.getCartById(cartId);
-
-        if (cart) {
-            res.json(cart);
-        } else {
-            res.status(404).json({ error: 'Carrito no encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener carrito' });
-    }
-});
-
-cartRouter.post('/:cid/product/:pid', async (req, res) => {
-    const cartId = req.params.cid;
-    const productId = req.params.pid;
-
-    try {
-        const addedProduct = await cartManager.addProductToCart(cartId, productId);
-        res.json(addedProduct);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al agregar producto al carrito' });
-    }
-});
-
-app.use('/api/carts', cartRouter);
-
-app.listen(port, () => {
+const server = httpServer.listen(port, () => {
     console.log(`Servidor corriendo en ${serverUrl}`);
 });
-
-async function testGET(url) {
-    try {
-        const response = await axios.get(url);
-        console.log('GET:', response.data);
-    } catch (error) {
-        console.error('Error en GET:', error.response.data);
-    }
-}
-
-async function testPOST(url, data) {
-    try {
-        const response = await axios.post(url, data);
-        console.log('POST:', response.data);
-    } catch (error) {
-        console.error('Error en POST:', error.response.data);
-    }
-}
-
-async function testPUT(url, data) {
-    try {
-        const response = await axios.put(url, data);
-        console.log('PUT:', response.data);
-    } catch (error) {
-        console.error('Error en PUT:', error.response.data);
-    }
-}
-
-async function testDELETE(url) {
-    try {
-        const response = await axios.delete(url);
-        console.log('DELETE:', response.data);
-    } catch (error) {
-        console.error('Error en DELETE:', error.response.data);
-    }
-}
